ce092d5cacaa1da2934339adf5ad02184033352f
duclk <lekhanhduccc@gmail.com>
2026-04-05 02:48:13 +0700

    Fix getUsername() in UserPrincipal to return user email instead

--- src/main/java/com/javabuilder/mybatis/security/UserPrincipal.java
+++ src/main/java/com/javabuilder/mybatis/security/UserPrincipal.java
@@ -34,7 +34,7 @@ public class UserPrincipal implements UserDetails {
 
     @Override
     public String getUsername() {
-        return getUsername();
+        return user.getEmail();
     }
 
     @Override
